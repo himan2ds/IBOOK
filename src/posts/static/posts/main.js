9d4169e7a466bf282c22f2b2bf98b266e09190d9
himanshu <himan2ds@gmail.com>
2021-05-10 22:57:14 +0530

    load_post_data_view_By_ serializer

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,8 @@
 console.log("Hello Baba")
+
 const helloWorldBox = document.getElementById('Hello--World')
 // helloWorldBox.innerHTML = "hiii <b>shaktimaan<b>"
+
 
 $.ajax({
     type : 'GET',
@@ -12,4 +14,18 @@ $.ajax({
     error: function(error){
         console.log('error',error);     
     }
+})
+
+$.ajax({
+    type : 'GET',
+    url : '/data/',
+    success : function(response){
+        console.log('success',response)
+        const data = JSON.parse(response.data)
+        console.log(data)
+    },
+    error: function(error){
+        console.log('error',error);
+    }
+
 })
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     post_list_and_create,
     hello_world_view,
+    load_post_data_view
 )
 
 app_name = 'posts'
@@ -9,4 +10,5 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('hw/', hello_world_view, name='hello-world'),
+    path('data/', load_post_data_view, name='posts-data'),
 ]
